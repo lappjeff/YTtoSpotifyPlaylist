eb7cb2d33acd867277d80513f8837bd1b9da4ed7
Jeff Lapp <lappjeff20@gmail.com>
2020-03-03 13:47:24 -0500

    Set up checkAuth route for checking auth status

--- BE/src/routes/auth/authRoutes.js
+++ BE/src/routes/auth/authRoutes.js
@@ -2,4 +2,16 @@ require("dotenv").config();
 const qs = require("query-string");
 const router = require("express").Router();
 
+const isAuthenticated = (req, res, next) => {
+	if (req.isAuthenticated) return next();
+	else {
+		return res.status(401).json({ error: "Unauthenticated" });
+	}
+};
+router.get("/checkAuth", isAuthenticated, (req, res) => {
+	res.status(200).json({
+		status: "Login Successful",
+		user: req.user
+	});
+});
 module.exports = router;
