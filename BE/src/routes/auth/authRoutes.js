6483c9155f5b106b298a5e2b7e2d1558a0c3e8e4
Jeff Lapp <lappjeff20@gmail.com>
2020-03-03 14:04:44 -0500

    Set up basic routes needed for Spotify auth

--- BE/src/routes/auth/authRoutes.js
+++ BE/src/routes/auth/authRoutes.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const qs = require("query-string");
 const router = require("express").Router();
+const passport = require("../../helpers/passport/passportConfig");
+
+const scope = ["user-read-private", "user-read-email"];
 
 const isAuthenticated = (req, res, next) => {
 	if (req.isAuthenticated) return next();
@@ -8,10 +11,31 @@ const isAuthenticated = (req, res, next) => {
 		return res.status(401).json({ error: "Unauthenticated" });
 	}
 };
+
 router.get("/checkAuth", isAuthenticated, (req, res) => {
 	res.status(200).json({
-		status: "Login Successful",
-		user: req.user
+		status: "Login Successful"
 	});
 });
+
+router.get(
+	"/",
+	passport.authenticate("spotify", {
+		scope,
+		failureRedirect: "http://localhost:3000",
+		showDialog: true
+	}),
+	(req, res) => {}
+);
+
+router.get(
+	"/callback",
+	passport.authenticate("spotify", { failureRedirect: "/authFail" }),
+	(req, res) => {
+		const tokenString = qs.stringify(req.user);
+		console.log(req.user);
+
+		res.redirect("checkAuth");
+	}
+);
 module.exports = router;
